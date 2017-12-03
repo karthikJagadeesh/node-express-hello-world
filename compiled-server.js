2a851e6a053353cd9e327bec2c8a8f3fad36df7e
Karthik Jagadeesha <kartu8@gmail.com>
2017-12-03 20:25:06 +0530

    explore

--- compiled-server.js
+++ compiled-server.js
@@ -1,16 +1,42 @@
 "use strict";
 
+require("babel-polyfill");
+
 var _express = require("express");
 
 var _express2 = _interopRequireDefault(_express);
 
+var _fs = require("fs");
+
+var _fs2 = _interopRequireDefault(_fs);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+var data = _fs2.default.readFileSync("./store.json");
+var words = JSON.parse(data);
+
 app.use(_express2.default.static("public"));
 
-var appStarted = function appStarted(_) {
-  console.log("App Started");
-};
+app.get("/all", function (request, response) {
+  return response.send(words);
+});
+app.get("/add/:word/:score", function (request, response) {
+  var params = request.params;
+  var word = params.word;
+  var score = params.score;
 
-var server = app.listen(3001, appStarted);
+  words[word] = +score;
+
+  _fs2.default.writeFile("./store.json", JSON.stringify(words, null, 2), function (_) {
+    response.send({
+      "status": "success",
+      "word": word,
+      "score": score
+    });
+  });
+});
+
+var server = app.listen(3000, function (_) {
+  return console.log("App Started");
+});
--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 import "babel-polyfill";
 import express from "express";
+import fs from "fs";
 
 const app = express();
+const data = fs.readFileSync("./store.json");
+const words = JSON.parse(data);
+
 app.use(express.static("public"));
 
-const appStarted = _ => {
-  console.log("App Started");
-};
+app.get("/all", (request, response) => response.send(words));
+app.get("/add/:word/:score", (request, response) => {
+  const params = request.params;
+  const word = params.word;
+  const score = params.score;
 
-const server = app.listen(3001, appStarted);
+  words[word] = +score;
+
+  fs.writeFile("./store.json", JSON.stringify(words, null, 2), _ => {
+    response.send({
+      "status": "success",
+      "word": word,
+      "score": score
+    });
+  })
+});
+
+const server = app.listen(3000, _ => console.log("App Started"));
